2f0077cbae43ce93dac0712f6f4696861a8eac91
Benjamin <benj.nuneza@duocuc.cl>
2024-09-08 21:47:38 -0300

    agregado de login diego

--- proyectoIonicDuoc/src/app/cursos/cursos.page.ts
+++ proyectoIonicDuoc/src/app/cursos/cursos.page.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.page.html',
   styleUrls: ['./cursos.page.scss'],
 })
-export class CursosPage implements OnInit {
+export class CursosPage   
+ implements OnInit {
 
-  constructor() { }
+  items:   
+ any[] = [];
+  totalItems: number = 10; // Adjust this to your desired limit
+  hasMoreData: boolean = true;
+
+  constructor() {}
 
   ngOnInit() {
+    this.GetItems();
   }
 
+  private GetItems(startIndex = 0, limit = 10) {
+    const newItems = []; // Create an empty array to store new items
+    const endIndex = startIndex + limit;
+
+    // Simulate fetching data from a backend (replace with your actual logic)
+    for (let i = startIndex; i < endIndex && i < this.totalItems; i++) {
+      newItems.push(`Item ${i + 1}`); // Adjust the item content as needed
+    }
+
+    this.items.push(...newItems); // Add new items to the existing list
+
+    // Check if all items have been loaded
+    this.hasMoreData = this.items.length < this.totalItems;
+  }
+
+  ionInfiniteScroll(ev: InfiniteScrollCustomEvent) {
+    const startIndex = this.items.length; // Start loading from the end of the current list
+
+    this.GetItems(startIndex); // Load more items
+
+    setTimeout(() => {
+      ev.target.complete(); // Let Ionic know data has been loaded
+    }, 1000); // Simulate a delay for a more realistic loading experience
+  }
 }
--- proyectoIonicDuoc/src/app/login/login.page.ts
+++ proyectoIonicDuoc/src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-login',
@@ -6,12 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  menuHome: boolean=true;
+  icono ="oscuro"
+  animarLogo: any;
   
-  constructor() { }
+  constructor(private anim:AnimationController) { }
 
   ngOnInit() {
+    this.animarLogo()
   }
+
+  cambiarTema(){
+    if(this.icono =="oscuro"){
+      document.documentElement.style.setProperty("--fondo","ffffff")
+      this.icono ="claro"
+    }else{
+      document.documentElement.style.setProperty("--fondo","#666666")
+      this.icono ="oscuro"
+  
+    }
+  }
+
+  animarError(index:number){
+    this.anim.create()
+    .addElement(document.querySelectorAll("input")[index])
+    .duration(200)
+    .iterations(3)
+    .keyframes([
+      {offset:0,transform:"translateX(0px)", boder: "1px transparent solid"},
+      {offset:0.25,transform:"translateX(-5px)", boder: "1px red solid"},
+      {offset:0.50,transform:"translateX(0px)", boder: "1px transparent solid"},
+      {offset:0.75,transform:"translateX(5px)", boder: "1px red solid"},
+      {offset:1,transform:"translateX(0px)", boder:  "1px transparent solid"},
+    ]).play()
+  }
+
 
 }
